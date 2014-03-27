3d3af843e5fabfde5d1740dda25a53cdfd2a697c
Dennis Molina <dennismolina.17@gmail.com>
2014-03-26 20:47:02 -0600

    add cards

--- MiniTrello.Api/Controllers/CardController.cs
+++ MiniTrello.Api/Controllers/CardController.cs
@@ -79,11 +79,11 @@ namespace MiniTrello.Api.Controllers
             var session = NewValidSession(Token);
             ValidateSession(session);
             var lane = _readOnlyRepository.GetById<Lane>(IdLane);
-            var mappedCardModelList = _mappingEngine.Map<IEnumerable<Card>, IEnumerable<CardModel>>(lane.Cards).ToList();
-            return mappedCardModelList.Where(card => !card.IsArchived).ToList();
+            //var mappedCardModelList = _mappingEngine.Map<IEnumerable<Card>, IEnumerable<CardModel>>(lane.Cards).ToList();
+            //return mappedCardModelList.Where(card => !card.IsArchived).ToList();
             //return mappedCardModelList;
-            //var cards = Builder<CardModel>.CreateListOfSize(10).Build().ToList();
-            //return cards;
+            var cards = Builder<CardModel>.CreateListOfSize(10).Build().ToList();
+            return cards;
         }
 
         public Sessions NewValidSession(string token)
